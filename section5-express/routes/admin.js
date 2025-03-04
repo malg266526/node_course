817ca5b9c7b2c1a720a8d9cb2b8329aecdf3caf7
malgo <malgtylek@gmail.com>
2025-03-04 14:47:52 +0100

    serve html files

--- section5-express/routes/admin.js
+++ section5-express/routes/admin.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require("node:path");
 
 const router = express.Router();
 
 router.get("/add-product", (req,res,next) => {
     console.log('In the middleware - Add Product');
-    // res.send('<h1>Hello from Add Product</h1>');
-    res.send('<form action="/add-product" method="post"><input type="text" name="title" ><button type="submit">Add product</button>  </form> ')
+
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 })
 
 router.post("/add-product", (req,res,next) => {

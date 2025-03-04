d75d0ae3b5e113076793893a51d97adcb5abc2e2
malgo <malgtylek@gmail.com>
2025-03-04 13:39:23 +0100

    get request body

--- section5-express/app.js
+++ section5-express/app.js
@@ -1,12 +1,23 @@
 const http = require('http');
+const bodyParser = require('body-parser');
 
 const express = require('express');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use("/add-product", (req,res,next) => {
     console.log('In the middleware - Add Product');
-    res.send('<h1>Hello from Add Product</h1>');
+    // res.send('<h1>Hello from Add Product</h1>');
+    res.send('<form action="/product" method="post"><input type="text" name="title" ><button type="submit">Add product</button>  </form> ')
+})
+
+app.post("/product", (req,res,next) => {
+    console.log('In the middleware - Product');
+
+    console.log('body', req.body);
+    res.redirect('/');
 })
 
 app.use("/", (req,res,next) => {

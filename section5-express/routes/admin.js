0986ca1a4e6cc36fe0867471f0f8e8ddaa729962
malgo <malgtylek@gmail.com>
2025-03-04 14:22:41 +0100

    filtering routes

--- section5-express/routes/admin.js
+++ section5-express/routes/admin.js
@@ -5,10 +5,10 @@ const router = express.Router();
 router.get("/add-product", (req,res,next) => {
     console.log('In the middleware - Add Product');
     // res.send('<h1>Hello from Add Product</h1>');
-    res.send('<form action="/product" method="post"><input type="text" name="title" ><button type="submit">Add product</button>  </form> ')
+    res.send('<form action="/add-product" method="post"><input type="text" name="title" ><button type="submit">Add product</button>  </form> ')
 })
 
-router.post("/product", (req,res,next) => {
+router.post("/add-product", (req,res,next) => {
     console.log('In the middleware - Product');
 
     console.log('body', req.body);

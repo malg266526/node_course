f27e4a5b1eeee6fcfa4faa559bd873673273b443
malg266526 <tylek.malgorzata@interia.pl>
2025-03-10 14:56:50 +0100

    reuse ejs components

--- shopApp/routes/shop.js
+++ shopApp/routes/shop.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const path = require('path');
 
-const adminData =  require('./admin');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get("/", (req,res,next) => {
+router.get("/", (req, res, next) => {
     const products = adminData.products;
 
     console.log('Show: products ', products);
 
-    res.render('shop', {products: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true});
+    res.render('shop', {
+        products: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
 })
 
 module.exports = router;
